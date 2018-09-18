6ec030fe23d2279462feb44da47d1abad71f1ee0
L Vicencio <lvicencio@gmail.com>
2018-09-18 01:47:39 -0300

    modelo y migracion de carrito y carrito detalle

---
+++ database/migrations/2018_09_18_041438_create_carritos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCarritosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carritos', function (Blueprint $table) {
+            $table->increments('id');
+
+            $table->date('order_date');
+            $table->date('arrived_date');
+            $table->string('status');
+
+            $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carritos');
+    }
+}
